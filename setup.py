c93b4c32d8e96a31094f70a2a6ba66f578a953a4
kisozinov <cirill2001@bk.ru>
2022-12-15 10:58:57 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='recsys-gcn-gan',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repository implements GCN and GAN architectures for recommeder systems.',
+    author='Kirill Sozinov, Andrey Matveev',
+    license='',
+)
